719a63f70c72ace9d96a0c08f5b6433d386e515a
Monique Dias <moniquedbandas@gmail.com>
2025-04-24 23:36:36 -0300

    Middleware global criado.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
+import express from "express";
+import { myMiddleware } from "./middlewares/my-middleware";
+//como esta usando o TS, nao precisa colocar a extensão
 
-const result: number = sum(5, 3);
-console.log("resultado da soma", result);
+const PORT = 3333;
+const app = express();
+//estou inicializando o express e colocando dentro da constante app todos os recursos que vamos ter diposniveis do express.
+app.use(express.json());
+app.use(myMiddleware);
+
+app.get("/products", (request, response) => {
+  const { page, limit } = request.query;
+  response.send(`Página ${page} de ${limit}`);
+});
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body;
+  response.status(201).json({ name, price });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
